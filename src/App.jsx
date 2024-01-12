59099804c322816cfa4687de0eaa725a70dd2f57
Blacklion567 <naviedaj567@gmail.com>
2024-01-12 16:38:26 +0800

    6_redux_basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import ReactBasics from "./components/01 React Basics/React.Basics";
-import ReactHooks from "./components/01 React Basics/ReactHooks";
-import ReactUseCallback from "./components/01 React Basics/ReactUseCallback";
-import ReactUseRef from "./components/01 React Basics/ReactUseRef";
 
-const App = () => {
+function App() {
+
   return (
     <>
-      <ReactBasics />
-      <ReactHooks />
-      <ReactUseRef />
-      <ReactUseCallback />
     </>
   );
-};
+}
 
 export default App;
